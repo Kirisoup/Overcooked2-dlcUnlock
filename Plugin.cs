c9393973427372658513ba5ac83c74b0c3f82c45
KirisameSoup <kirisoup@skiff.com>
2024-04-29 11:44:51 +0800

    yes

--- Plugin.cs
+++ Plugin.cs
@@ -6,9 +6,7 @@ namespace ocdlcu
         [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
         public class Plugin : BaseUnityPlugin
         {
-                Harmony harmony;
-
-                void Awake() => harmony = Harmony.CreateAndPatchAll(typeof(Plugin), "com.kirisoup.divource.ocdlcu");
+                void Awake() => Harmony.CreateAndPatchAll(typeof(Plugin), "com.kirisoup.divource.ocdlcu");
 
                 [HarmonyPatch(typeof(DLCManagerBase), "IsDLCAvailable")]
                 static void Postfix(ref bool __result) => __result = true;
